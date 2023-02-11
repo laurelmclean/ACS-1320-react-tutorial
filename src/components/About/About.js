d77b6577b3a9bda635234b28e7c34dde72dc23c5
Laurel McLean <laurel.mclean@live.ca>
2023-02-11 15:07:12 -0700

    Finished assignment

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,17 +1,27 @@
 import React from 'react'
+import './About.css'
 
 function About() {
   return (
-    <div>
-      <h1>About SFPOPOS</h1>
-      <p>POPOS are publicly accessible spaces in
+    <div className='About'>
+      <div className="About-image">
+        <img src={`${process.env.PUBLIC_URL}/images/50-california-st.jpg`}     
+       
+        height="auto"
+        alt="50 Califonia St."
+      />
+      </div>
+      <div className="About-info">
+      <h1 className='About-title'>About SFPOPOS</h1>
+      <p className='About-desc'>POPOS are publicly accessible spaces in
         forms of plazas, terraces, atriums, small
         parks, and even snippets which are provided
         and maintained by private developers. In San
         Francisco, POPOS mostly appear in the Downtown
         office district area.</p>
+        </div>
     </div>
   )
 }
 
-export default About
+export default About;
